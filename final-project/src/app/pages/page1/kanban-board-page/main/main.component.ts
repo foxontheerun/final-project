46372b7cfdfa79103d5c1c6b318e084cd1396273
alsu Bulatova <bulatovaalsu22@gmail.com>
2023-02-08 03:27:56 +0300

    Редактирование Main component

--- final-project/src/app/pages/page1/kanban-board-page/main/main.component.ts
+++ final-project/src/app/pages/page1/kanban-board-page/main/main.component.ts
@@ -7,24 +7,11 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  incomingRequests = ['Ошибка датчика КП-312', 'Тех обслуживание насоса ВР-231', 'Авария на ТП-123'];
-  inWorkRequests = ['Подключение системы ', 'Поверка датчиков на УПН-231'];
-  toAgreedRequests = ['Обновление ПО'];
-  doneRequests = ['Корректировка значений по ВКУ-321'];
-  forShipmentRequests = []
-
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
-  }
+  public incomingRequests = ['Ошибка датчика КП-312', 'Тех обслуживание насоса ВР-231', 'Авария на ТП-123'];
+  public inWorkRequests = ['Подключение системы ', 'Поверка датчиков на УПН-231'];
+  public toAgreedRequests = ['Обновление ПО'];
+  public doneRequests = ['Корректировка значений по ВКУ-321'];
+  public forShipmentRequests = [];
 
   constructor() { }
 
